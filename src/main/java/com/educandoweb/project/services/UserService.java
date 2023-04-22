acd415f95c4ad9584573df196dc55a76a7c72ea3
Julio Donofreo <julio.bibico@hotmail.com>
2023-04-21 21:46:53 -0300

    Service layer and componet registration

---
+++ src/main/java/com/educandoweb/project/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.project.entities.User;
+import com.educandoweb.project.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> finAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
+	}
+}
+
