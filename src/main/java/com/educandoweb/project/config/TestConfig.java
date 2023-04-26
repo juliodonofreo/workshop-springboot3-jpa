dbebd9dbd9bd4ad1116e6142bd3ecf00e17b8d2f
Julio Donofreo <julio.bibico@hotmail.com>
2023-04-25 21:56:12 -0300

    Payment entity implemented with one to one relationship

--- src/main/java/com/educandoweb/project/config/TestConfig.java
+++ src/main/java/com/educandoweb/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.project.entities.Category;
 import com.educandoweb.project.entities.Order;
 import com.educandoweb.project.entities.OrderItem;
+import com.educandoweb.project.entities.Payment;
 import com.educandoweb.project.entities.Product;
 import com.educandoweb.project.entities.User;
 import com.educandoweb.project.entities.enums.OrderStatus;
@@ -34,7 +35,7 @@ public class TestConfig implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	
 	@Autowired
-	ProductRepository productRepository;
+	private ProductRepository productRepository;
 	
 	@Autowired
 	private OrderItemRepository orderItemRepository;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
